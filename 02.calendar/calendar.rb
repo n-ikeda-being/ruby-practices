544eaabca67c21cb4b5da276b3e59420419156e9
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-09-14 10:08:55 +0900

    Date.newの冗長構成見直し・後置ifに修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,43 +1,37 @@
 require 'date'
 require 'optparse'
 
-year_today = Date.today.year
-month_today = Date.today.mon
+year_def = Date.today.year
+month_def = Date.today.mon
 
 input = ARGV.getopts("y:", "m:")
 
-year =
-  if input["y"]
-    input["y"].to_i
-  else
-    year_today
-  end
-
-month =
-if input["m"]
-  input["m"].to_i
+if input["y"]
+  year = input["y"].to_i
 else
-  month_today
+  year = year_def
+end
+
+if input["m"]
+  month = input["m"].to_i
+else
+  month = month_def
 end
 
 title = "#{year}年#{month}月"
-first_date = Date.new(year,month,1).day
+first_day_of_month = Date.new(year,month,1)
+first_date = first_day_of_month.day
 last_date = Date.new(year,month,-1).day
-firstday_dow = Date.new(year,month,1).wday
+firstday_dow = first_day_of_month.wday
 week = ["日","月","火","水","木","金","土"]
 
 puts title.center(20)
 puts week.join(" ")
 print "   " * firstday_dow
 
-wday = firstday_dow
-(first_date..last_date).each do |date|
+dow = firstday_dow
+(1..last_date).each do |date|
   print date.to_s.rjust(2) + " "
-  wday = wday + 1
-  if wday % 7 == 0    #土曜日(7の倍数)で改行
-    puts
-  end
-end
-if wday % 7 != 0
-  puts
+  dow = dow + 1
+  puts if dow % 7 == 0    #土曜日(7の倍数)で改行
 end
