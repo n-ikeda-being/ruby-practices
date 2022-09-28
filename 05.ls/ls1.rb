364eafab222441892a4fa9749ed369b93c8e0064
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-09-28 09:25:59 +0900

    変数・定数名を一部変更した

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -3,42 +3,36 @@
 
 all_files = Dir.glob('*')
 
-MAX_COLUMN = 3
+MAX_NUMBER_OF_COLUMN = 3
 BETWEEN_FILES_SPACE = 3
 
 def main(all_files)
   row = row(all_files)
-#ファイル数が１～９個の場合は縦４列
-  row = 4 if all_files.size <= 9
-  max_word_file = max_file_size(all_files)
+  max_file_size = max_file_size(all_files)
   splitted_files = split_file(all_files, row)
-  output(row, max_word_file, splitted_files)
+  output(row, max_file_size, splitted_files)
 end
 
-#縦を何行にするか計算する
 def row(all_files)
   all_file_quantity = all_files.size
-  (all_file_quantity.to_f / MAX_COLUMN).ceil
+  (all_file_quantity.to_f / MAX_NUMBER_OF_COLUMN).ceil
 end
 
-#１番桁数の多いファイル名の桁数を取得
 def max_file_size(all_files)
   all_files.map(&:length).max
 end
 
-#rowの数ずつ分割してグループ化
 def split_file(all_files, row)
   all_files.each_slice(row).map
 end
 
-def output(row, max_word_file, splitted_files)
+def output(row, max_file_size, splitted_files)
   row.times do |i|
     splitted_files.each do |files|
-      print files[i].ljust(max_word_file + BETWEEN_FILES_SPACE) unless files[i].nil?
+      print files[i].ljust(max_file_size + BETWEEN_FILES_SPACE) unless files[i].nil?
     end
     puts
   end
 end
 
 main(all_files)
-
