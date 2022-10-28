62e366d3561ed0ea24dc93b215885895dd673216
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-10-28 13:45:14 +0900

    fs.mode.digits(8)を変数化とmainの中でオプション有・無を分岐

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -6,11 +6,6 @@ require 'date'
 
 all_files = Dir.glob('*')
 
-opt = OptionParser.new
-params = {}
-opt.on('-l') { |v| params[:l] = v }
-opt.parse!(ARGV)
-
 MAX_NUMBER_OF_COLUMN = 3
 BETWEEN_FILES_SPACE = 3
 
@@ -23,7 +18,23 @@ GROUP_AUTHORITY_NUMBER = 1
 OTHER_AUTHORITY_NUMBER = 0
 
 # オプションなし
+
 def main(all_files)
+  argv_option = parse_option(ARGV)
+  no_option(all_files) unless argv_option[:l]
+  l_option(all_files) if argv_option[:l]
+end
+
+def parse_option(argv)
+  argv_option = {}
+  OptionParser.new do |opt|
+    opt.on('-l') { |v| argv_option[:l] = v }
+    opt.parse!(argv)
+  end
+  argv_option
+end
+
+def no_option(all_files)
   row = row(all_files)
   max_file_size = max_file_size(all_files)
   splitted_files = split_file(all_files, row)
@@ -52,31 +63,32 @@ def output(row, max_file_size, splitted_files)
   end
 end
 
-def l_main(all_files)
-  loutput(all_files)
-end
-
-def loutput(all_files)
+def l_option(all_files)
   all_files.map do |files|
     fs = File.lstat(files)
-    file_type = fs.mode.digits(8)[4].to_s
-    file_type = fs.mode.digits(8)[5].to_s + fs.mode.digits(8)[4].to_s if fs.mode.digits(8)[5] == 1
+    octal_file_mode = fs.mode.digits(8)
+    octal_file_type = fs.mode.digits(8)[5]
+    file_type =
+      if octal_file_type == 1
+        octal_file_type.to_s + octal_file_mode[4].to_s
+      else
+        octal_file_mode[4].to_s
+      end
     output_file_type = FILE_TYPE[file_type]
-    user_authority = FILE_AUTHORITY[fs.mode.digits(8)[USER_AUTHORITY_NUMBER].to_s]
-    group_authority = FILE_AUTHORITY[fs.mode.digits(8)[GROUP_AUTHORITY_NUMBER].to_s]
-    other_authority = FILE_AUTHORITY[fs.mode.digits(8)[OTHER_AUTHORITY_NUMBER].to_s]
+    user_authority = FILE_AUTHORITY[octal_file_mode[USER_AUTHORITY_NUMBER].to_s]
+    group_authority = FILE_AUTHORITY[octal_file_mode[GROUP_AUTHORITY_NUMBER].to_s]
+    other_authority = FILE_AUTHORITY[octal_file_mode[OTHER_AUTHORITY_NUMBER].to_s]
 
     puts
     print output_file_type.to_s + user_authority.to_s + group_authority.to_s + other_authority.to_s
-    print "  #{fs.nlink}"
-    print "  #{Etc.getpwuid(fs.uid).name}"
-    print "  #{Etc.getgrgid(fs.gid).name}"
-    print "  #{fs.size.to_s.rjust(5)}"
-    print "  #{fs.mtime.strftime('%-m月 %d %H:%M')}"
-    print "  #{files.rjust(5)}"
+    print " #{fs.nlink}"
+    print " #{Etc.getpwuid(fs.uid).name}"
+    print " #{Etc.getgrgid(fs.gid).name}"
+    print " #{fs.size.to_s.rjust(5)}"
+    print " #{fs.mtime.strftime('%-m月 %d %H:%M')}"
+    print " #{files.rjust(5)}"
   end
   puts
 end
 
-l_main(all_files) if params[:l]
-main(all_files) unless params[:l]
+main(all_files)
